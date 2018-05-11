d0876964426d108043d1877591715996f79e8d92
= <tarasevich.vlad.97@gmail.com>
2018-05-11 12:58:35 +0300

    add memory

---
+++ RW_RAM_BUSTRI_inst.vhd
@@ -0,0 +1,7 @@
+RW_RAM_BUSTRI_inst : RW_RAM_BUSTRI PORT MAP (
+		data	 => data_sig,
+		enabledt	 => enabledt_sig,
+		enabletr	 => enabletr_sig,
+		result	 => result_sig,
+		tridata	 => tridata_sig
+	);
