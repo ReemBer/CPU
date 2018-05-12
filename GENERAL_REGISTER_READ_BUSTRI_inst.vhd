634f2bbb6b81b74f236985502426bc2cfde5baa6
= <tarasevich.vlad.97@gmail.com>
2018-05-12 04:44:33 +0300

    add general registers

---
+++ GENERAL_REGISTER_READ_BUSTRI_inst.vhd
@@ -0,0 +1,5 @@
+GENERAL_REGISTER_READ_BUSTRI_inst : GENERAL_REGISTER_READ_BUSTRI PORT MAP (
+		data	 => data_sig,
+		enabledt	 => enabledt_sig,
+		tridata	 => tridata_sig
+	);
---
+++ GENERAL_REGISTER_inst.vhd
@@ -0,0 +1,6 @@
+GENERAL_REGISTER_inst : GENERAL_REGISTER PORT MAP (
+		clock	 => clock_sig,
+		data	 => data_sig,
+		enable	 => enable_sig,
+		q	 => q_sig
+	);
