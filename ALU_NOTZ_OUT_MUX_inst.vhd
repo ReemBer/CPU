4a910e5ca9f7824d7759333c2f4bf6c0058dff01
= <tarasevich.vlad.97@gmail.com>
2018-05-15 18:35:08 +0300

    add NOTZDR

---
+++ ALU_NOTZ_OUT_MUX_inst.vhd
@@ -0,0 +1,6 @@
+ALU_NOTZ_OUT_MUX_inst : ALU_NOTZ_OUT_MUX PORT MAP (
+		data0x	 => data0x_sig,
+		data1x	 => data1x_sig,
+		sel	 => sel_sig,
+		result	 => result_sig
+	);
---
+++ ALU_NOTZ_REGISTER_inst.vhd
@@ -0,0 +1,6 @@
+ALU_NOTZ_REGISTER_inst : ALU_NOTZ_REGISTER PORT MAP (
+		clock	 => clock_sig,
+		data	 => data_sig,
+		enable	 => enable_sig,
+		q	 => q_sig
+	);
